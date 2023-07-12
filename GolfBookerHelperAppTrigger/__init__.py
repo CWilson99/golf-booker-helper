6bffa269564ab275ba278d20d055c40b6765cf42
Taubs99 <connorjwilson99@hotmail.com>
2023-07-12 13:07:48 +1000

    deserializing json

--- GolfBookerHelperAppTrigger/__init__.py
+++ GolfBookerHelperAppTrigger/__init__.py
@@ -82,7 +82,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     date = get_param(req, 'date')
-    site = get_param(req, 'site')
+    site = json.load(get_param(req, 'site'))
 
     logging.info(site)
 
